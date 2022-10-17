eabe70577e0e3bbdf6478e8a509c4dc15f5a0b23
Dmitry Orlov <dmitryorly611@gmail.com>
2022-10-17 11:24:00 +0300

    refactor: cleanup

--- src/pages/music.js
+++ src/pages/music.js
@@ -18,13 +18,10 @@ function Music({ audioFiles }) {
 export default Music
 
 export async function getStaticProps() {
-	// const response = await fetch(`${API_URL}/music/kodo.mp3`)
-	// const buffer = await response.arrayBuffer()
-	// const waveformData = await waveform(buffer)
-
 	const audioFiles = [
-		// eslint-disable-next-line max-len
-		'https://downloads.ctfassets.net/4b8maak9frxn/1DdbYrSgOGPQtHC0UoIpWZ/591214e50d6088ae4fef21b5c101abaf/2020-02-16_23.16.58.mp3'
+		'https://downloads.ctfassets.net' +
+			'/4b8maak9frxn/1DdbYrSgOGPQtHC0UoIpWZ' +
+			'/591214e50d6088ae4fef21b5c101abaf/2020-02-16_23.16.58.mp3'
 	]
 	const props = { audioFiles }
 
