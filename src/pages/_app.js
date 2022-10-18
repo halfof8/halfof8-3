d39a29ed62d9726da21b9b950fdb5e0014b2af29
Dmitry Orlov <dmitryorly611@gmail.com>
2022-10-18 14:37:17 +0300

    fix: styles removed too early on route change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,16 +4,22 @@ import { AnimatePresence, domAnimation, LazyMotion, m } from 'framer-motion'
 import { fadeIn, slideUp } from '../lib/animations'
 import Header from '../components/Header/Header.js'
 import { useState } from 'react'
+import { useTransitionFix } from '../hooks/useTransitionFix.js'
 
 function App({ Component, pageProps, router }) {
 	const [headerDisabled, setHeaderDisabled] = useState(false)
+	const transitionCallback = useTransitionFix()
+	const onExitComplete = () => {
+		setHeaderDisabled(false)
+		transitionCallback()
+	}
 
 	return (
 		<div className={css.app}>
 			<Header disabled={headerDisabled} />
 
 			<LazyMotion features={domAnimation} strict>
-				<AnimatePresence exitBeforeEnter={false} initial={false} onExitComplete={() => setHeaderDisabled(false)}>
+				<AnimatePresence exitBeforeEnter={false} initial={false} onExitComplete={onExitComplete}>
 					<m.div
 						className={css.wrap}
 						key={router.route}
