8840457c8ea1005375071ddcff7e5779c963095d
Dmitry Orlov <dmitryorly611@gmail.com>
2022-10-25 15:58:07 +0300

    wip: timeseek component

---
+++ src/components/Timeseek/Timeseek.js
@@ -0,0 +1,11 @@
+import css from './Timeseek.module.scss'
+
+const Timeseek = () => {
+	return (
+		<div className={css.icon}>
+			<div className={css.circle} />
+		</div>
+	)
+}
+
+export default Timeseek
